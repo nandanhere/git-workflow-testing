9e08f2df5766a5bd9664cfe7e4ae17430f839b54
Nandan <nanspyro@gmail.com>
2021-08-31 19:32:55 +0530

    Test for spec files

--- build/android/buildApk.sh
+++ build/android/buildApk.sh
@@ -28,33 +28,16 @@ install os-level depends
 
 pip install buildozer
 pip install cython
-
 sudo apt-get update; sudo apt-get -y install python3.7 python3-pip python3-setuptools wget rsync fuse
 sudo apt-get install -y build-essential git python3-dev ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
-sudo apt-get install aidl
-
-
 sudo apt-get install -y libgstreamer1.0  gstreamer1.0-plugins-base gstreamer1.0-plugins-good
 sudo apt-get install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev libffi6
 sudo apt-get install libffi-dev
-
-
 
 uname -a
 cat /etc/issue
 which python
 which python3.7
-
-
-
-# setup a virtualenv to isolate our app's python depends
-#${PYTHON_PATH} -m pip install --upgrade --user pip setuptools
-#${PYTHON_PATH} -m pip install --upgrade --user virtualenv
-#${PYTHON_PATH} -m virtualenv /tmp/kivy_venv
-
-# install kivy and all other python dependencies with pip into our virtual env
-# we'll later add these to our AppDir for building the AppImage
-#source /tmp/kivy_venv/bin/activate; python -m pip install -r requirements.txt
 
 ##################
 # PREPARE APPDIR #
@@ -63,27 +46,13 @@ which python3.7
 # cleanup old appdir, if exists
 
 
-# #! /bin/bash
-# cd /home/runner/.buildozer/android/platform/
-
-# wget -P /home/runner/.buildozer/android/platform/android-sdk/ https://dl.google.com/android/repository/commandlinetools-linux-6514223_latest.zip 
-# echo "unzipping"
-# cd /home/runner/.buildozer/android/platform/android-sdk/
-
-# unzip -q commandlinetools-linux-6514223_latest.zip 
-# cd /home/runner/.buildozer/android/platform/android-sdk/
-# ls
-# #! /usr/bin/expect -f
-# yes | /home/runner/.buildozer/android/platform/android-sdk/tools/bin/sdkmanager --sdk_root=/home/runner/.buildozer/android/platform/android-sdk "build-tools;31.0.0"
-
 
 # Export APPRUN if running from an extracted image
 cd src
-buildozer init y
+echo y | buildozer init 
+rm buildozer.spec
+mv build/android/buildozer.spec buildozer.spec
 echo y | buildozer android debug 
-
-
-
 
 exit "$?"
 EOF

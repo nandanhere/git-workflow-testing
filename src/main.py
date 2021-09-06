16cc275cfa5331ada6f6f346b0b094f91808c307
Nandan <nanspyro@gmail.com>
2021-09-06 11:35:18 +0530

    Example file added

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world!')
+        return Label(text='Hello world! This was created in a github workflow!')
 
 
 if __name__ == '__main__':
